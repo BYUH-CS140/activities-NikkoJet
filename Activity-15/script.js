1488592df46071ab617ad2600fe6cd4c403b9fdc
NikkoJet <nikkojet@go.byuh.edu>
2025-06-14 19:40:42 -1000

    java

--- Activity-15/script.js
+++ Activity-15/script.js
@@ -1,4 +1,5 @@
 document.getElementById('changeColorButton').addEventListener('click', function() {
+    document.body.style.backgroundImage = "none"; // remove the background image
     document.body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
   });
   
